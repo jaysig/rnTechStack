db7c75a69a7757c4acbb1792e901e95948b2a4b5
Jaysig <jasontsigmon@gmail.com>
2017-03-07 17:02:03 -0600

    Starting project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Album List!!!</Text>
+    </View>
+  );
+};
+
+export default App;
+// export { A }
