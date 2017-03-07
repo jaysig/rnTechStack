bbb1873ff19de05f9e37a3f389cb1fe2a0400e9b
Jaysig <jasontsigmon@gmail.com>
2017-03-07 17:31:43 -0600

    Added JSON data

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-    <View>
-      <Text>Album List!!!</Text>
-    </View>
+      <View>
+        <Header headerText="Tech Stack" />
+        <Text>Album List!!!</Text>
+      </View>
     </Provider>
   );
 };
