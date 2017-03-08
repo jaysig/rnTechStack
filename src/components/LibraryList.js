bea248045484ed0da270d849eb79880b1445da5f
Jaysig <jasontsigmon@gmail.com>
2017-03-07 18:04:20 -0600

    Starting ListItem

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import { connect } from 'react-redux';
+
 // import * as actions from '../../actions';
 
 class LibraryList extends Component {
 
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+  renderRow() {
+    
+  }
   render() {
     return (
-      <View>
-        <Text>This is a feature</Text>
-      </View>
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
     );
   }
 }
 const mapStateToProps = state => {
- return { authenticated: state.auth.authenticated };
+ return { libraries: state.libraries };
 };
 // mapStateToProps, actions
 export default connect(mapStateToProps)(LibraryList);
---
+++ src/components/ListItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+// import { connect } from 'react-redux';
+// import * as actions from '../../actions';
+
+class ListItem extends Component {
+
+  render() {
+    return (
+      <View style={}>
+        <Text>This is a feature</Text>
+      </View>
+    );
+  }
+}
+// function mapStateToProps(state) {
+//  return { authenticated: state.auth.authenticated };
+// }
+// export default connect(mapStateToProps, actions)(Header)
+export default ListItem;
