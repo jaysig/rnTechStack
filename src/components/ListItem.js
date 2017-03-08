ff6b8f3edacccfd5e5f0dcb2c171c6d03c6e2798
Jaysig <jasontsigmon@gmail.com>
2017-03-08 15:27:18 -0600

    Improved SelectionReducer

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
 import { CardSection } from './common';
-// import { connect } from 'react-redux';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
 // import * as actions from '../../actions';
 
 class ListItem extends Component {
 
   render() {
     const { titleStyle } = styles;
+    const { id, title } = this.props.library;
+    
     return (
-      <CardSection>
-        <Text style={titleStyle}>{this.props.library.title}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>{title}</Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -25,5 +34,5 @@ const styles = {
 // function mapStateToProps(state) {
 //  return { authenticated: state.auth.authenticated };
 // }
-// export default connect(mapStateToProps, actions)(Header)
-export default ListItem;
+
+export default connect(null, actions)(ListItem);
